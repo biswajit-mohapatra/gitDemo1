625625df3035c548af189aa0ebee459380d0d603
Biswajit Mohapatra <biswajit.javaexpert@gmail.com>
2020-07-12 00:07:00 +0530

    Was added for demo purpose

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
   public static void main(String[] args) {
     System.out.println("Demo to add a project into github through Intellij Idea");
+    System.out.println("Successfully imported it into Github");
+    System.out.println("Finished");
   }
 }
